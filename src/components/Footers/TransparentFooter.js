29a85020eda5fce055eba2029e6ad789b9be0252
infant_sabin <a-sabin@mithrai.com>
2020-01-21 19:28:34 +0530

    changes in Footer

--- src/components/Footers/TransparentFooter.js
+++ src/components/Footers/TransparentFooter.js
@@ -8,48 +8,14 @@ function TransparentFooter() {
   return (
     <footer className="footer">
       <Container>
-        <nav>
-          <ul>
-            <li>
-              <a
-                href="https://www.creative-tim.com?ref=nukr-transparent-footer"
-                target="_blank"
-              >
-                Creative Tim
-              </a>
-            </li>
-            <li>
-              <a
-                href="http://presentation.creative-tim.com?ref=nukr-transparent-footer"
-                target="_blank"
-              >
-                About Us
-              </a>
-            </li>
-            <li>
-              <a
-                href="http://blog.creative-tim.com?ref=nukr-transparent-footer"
-                target="_blank"
-              >
-                Blog
-              </a>
-            </li>
-          </ul>
-        </nav>
         <div className="copyright" id="copyright">
-          © {new Date().getFullYear()}, Designed by{" "}
+          © {new Date().getFullYear()},
+          Coded by{" "}
           <a
-            href="https://www.invisionapp.com?ref=nukr-transparent-footer"
+            href="https://in.linkedin.com/in/infant-sabin-a-788749149"
             target="_blank"
           >
-            Invision
-          </a>
-          . Coded by{" "}
-          <a
-            href="https://www.creative-tim.com?ref=nukr-transparent-footer"
-            target="_blank"
-          >
-            Creative Tim
+            Infant Sabin
           </a>
           .
         </div>
