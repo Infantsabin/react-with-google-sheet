cc0a00067f3eab435eaaccb28c148051cd015909
infant_sabin <a-sabin@mithrai.com>
2020-01-21 19:00:30 +0530

    Changes in NavBar

--- src/components/Navbars/IndexNavbar.js
+++ src/components/Navbars/IndexNavbar.js
@@ -1,13 +1,7 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
 // reactstrap components
 import {
-  Button,
   Collapse,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  UncontrolledDropdown,
   NavbarBrand,
   Navbar,
   NavItem,
@@ -15,165 +9,111 @@ import {
   Nav,
   Container,
   UncontrolledTooltip
-} from "reactstrap";
+} from 'reactstrap'
 
-function IndexNavbar() {
-  const [navbarColor, setNavbarColor] = React.useState("navbar-transparent");
-  const [collapseOpen, setCollapseOpen] = React.useState(false);
+function IndexNavbar () {
+  const [navbarColor, setNavbarColor] = React.useState('navbar-transparent')
+  const [collapseOpen, setCollapseOpen] = React.useState(false)
   React.useEffect(() => {
     const updateNavbarColor = () => {
       if (
         document.documentElement.scrollTop > 399 ||
         document.body.scrollTop > 399
       ) {
-        setNavbarColor("");
+        setNavbarColor('')
       } else if (
         document.documentElement.scrollTop < 400 ||
         document.body.scrollTop < 400
       ) {
-        setNavbarColor("navbar-transparent");
+        setNavbarColor('navbar-transparent')
       }
-    };
-    window.addEventListener("scroll", updateNavbarColor);
-    return function cleanup() {
-      window.removeEventListener("scroll", updateNavbarColor);
-    };
-  });
+    }
+    window.addEventListener('scroll', updateNavbarColor)
+    return function cleanup () {
+      window.removeEventListener('scroll', updateNavbarColor)
+    }
+  })
   return (
     <>
       {collapseOpen ? (
         <div
-          id="bodyClick"
+          id='bodyClick'
           onClick={() => {
-            document.documentElement.classList.toggle("nav-open");
-            setCollapseOpen(false);
+            document.documentElement.classList.toggle('nav-open')
+            setCollapseOpen(false)
           }}
         />
       ) : null}
-      <Navbar className={"fixed-top " + navbarColor} expand="lg" color="info">
+      <Navbar className={'fixed-top ' + navbarColor} expand='lg' color='info'>
         <Container>
-          <div className="navbar-translate">
+          <div className='navbar-translate'>
             <NavbarBrand
-              href="https://demos.creative-tim.com/now-ui-kit-react/#/index?ref=nukr-index-navbar"
-              target="_blank"
-              id="navbar-brand"
+              href='https://github.com/Infantsabin/react-with-google-sheet'
+              target='_blank'
+              id='navbar-brand'
             >
-              Kaizzan
+              Neo
             </NavbarBrand>
-            <UncontrolledTooltip target="#navbar-brand">
-              Designed by Kaizzan. Coded by Infant Sabin
+            <UncontrolledTooltip target='#navbar-brand'>
+              Designed by Neo. Coded by Infant Sabin
             </UncontrolledTooltip>
             <button
-              className="navbar-toggler navbar-toggler"
+              className='navbar-toggler navbar-toggler'
               onClick={() => {
-                document.documentElement.classList.toggle("nav-open");
-                setCollapseOpen(!collapseOpen);
+                document.documentElement.classList.toggle('nav-open')
+                setCollapseOpen(!collapseOpen)
               }}
               aria-expanded={collapseOpen}
-              type="button"
+              type='button'
             >
-              <span className="navbar-toggler-bar top-bar"></span>
-              <span className="navbar-toggler-bar middle-bar"></span>
-              <span className="navbar-toggler-bar bottom-bar"></span>
+              <span className='navbar-toggler-bar top-bar' />
+              <span className='navbar-toggler-bar middle-bar' />
+              <span className='navbar-toggler-bar bottom-bar' />
             </button>
           </div>
           <Collapse
-            className="justify-content-end"
+            className='justify-content-end'
             isOpen={collapseOpen}
             navbar
           >
             <Nav navbar>
               <NavItem>
                 <NavLink
-                  href="#pablo"
-                  onClick={e => {
-                    e.preventDefault();
-                    document
-                      .getElementById("download-section")
-                      .scrollIntoView();
-                  }}
+                  href='https://in.linkedin.com/in/infant-sabin-a-788749149'
+                  target='_blank'
+                  id='linkedin-tooltip'
                 >
-                  <i className="now-ui-icons arrows-1_cloud-download-93"></i>
-                  <p>Download</p>
+                  <i className='fab fa-linkedin' />
+                  <p className='d-lg-none d-xl-none'>LinkedIn</p>
                 </NavLink>
-              </NavItem>
-              <UncontrolledDropdown nav>
-                <DropdownToggle
-                  caret
-                  color="default"
-                  href="#pablo"
-                  nav
-                  onClick={e => e.preventDefault()}
-                >
-                  <i className="now-ui-icons design_app mr-1"></i>
-                  <p>Components</p>
-                </DropdownToggle>
-                <DropdownMenu>
-                  <DropdownItem to="/index" tag={Link}>
-                    <i className="now-ui-icons business_chart-pie-36 mr-1"></i>
-                    All components
-                  </DropdownItem>
-                  <DropdownItem
-                    href="https://demos.creative-tim.com/now-ui-kit-react/#/documentation/introduction?ref=nukr-index-navbar"
-                    target="_blank"
-                  >
-                    <i className="now-ui-icons design_bullet-list-67 mr-1"></i>
-                    Documentation
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-              <NavItem>
-                <Button
-                  className="nav-link btn-neutral"
-                  color="info"
-                  href="#pablo"
-                  id="upgrade-to-pro"
-                  onClick={e => e.preventDefault()}
-                >
-                  <i className="now-ui-icons arrows-1_share-66 mr-1"></i>
-                  <p>Upgrade to PRO</p>
-                </Button>
-                <UncontrolledTooltip target="#upgrade-to-pro">
-                  Cooming soon!
+                <UncontrolledTooltip target='#linkedin-tooltip'>
+                  Follow me on LinkedIn
                 </UncontrolledTooltip>
               </NavItem>
               <NavItem>
                 <NavLink
-                  href="https://twitter.com/CreativeTim?ref=creativetim"
-                  target="_blank"
-                  id="twitter-tooltip"
+                  href='https://www.facebook.com/infant.sabinson.5'
+                  target='_blank'
+                  id='facebook-tooltip'
                 >
-                  <i className="fab fa-twitter"></i>
-                  <p className="d-lg-none d-xl-none">Twitter</p>
+                  <i className='fab fa-facebook-square' />
+                  <p className='d-lg-none d-xl-none'>Facebook</p>
                 </NavLink>
-                <UncontrolledTooltip target="#twitter-tooltip">
-                  Follow us on Twitter
+                <UncontrolledTooltip target='#facebook-tooltip'>
+                  Like me on Facebook
                 </UncontrolledTooltip>
               </NavItem>
               <NavItem>
                 <NavLink
-                  href="https://www.facebook.com/CreativeTim?ref=creativetim"
-                  target="_blank"
-                  id="facebook-tooltip"
+                  href='https://www.instagram.com/infantsabin/'
+                  target='_blank'
+                  id='instagram-tooltip'
                 >
-                  <i className="fab fa-facebook-square"></i>
-                  <p className="d-lg-none d-xl-none">Facebook</p>
+                  <i className='fab fa-instagram' />
+                  <p className='d-lg-none d-xl-none'>Instagram</p>
                 </NavLink>
-                <UncontrolledTooltip target="#facebook-tooltip">
-                  Like us on Facebook
-                </UncontrolledTooltip>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  href="https://www.instagram.com/CreativeTimOfficial?ref=creativetim"
-                  target="_blank"
-                  id="instagram-tooltip"
-                >
-                  <i className="fab fa-instagram"></i>
-                  <p className="d-lg-none d-xl-none">Instagram</p>
-                </NavLink>
-                <UncontrolledTooltip target="#instagram-tooltip">
-                  Follow us on Instagram
+                <UncontrolledTooltip target='#instagram-tooltip'>
+                  Follow me on Instagram
                 </UncontrolledTooltip>
               </NavItem>
             </Nav>
@@ -181,7 +121,7 @@ function IndexNavbar() {
         </Container>
       </Navbar>
     </>
-  );
+  )
 }
 
-export default IndexNavbar;
+export default IndexNavbar
